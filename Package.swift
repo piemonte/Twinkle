43e18ca26bb092b098a5ae5ae01973e26fd3e268
patrick piemonte <piemonte@users.noreply.github.com>
2020-06-18 23:06:18 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
 // swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
     name: "Twinkle",
     platforms: [
-      .iOS(.v10)
+      .iOS(.v10),
+      .tvOS(.v10)
     ],
     products: [
       .library(name: "Twinkle", targets: ["Twinkle"])
